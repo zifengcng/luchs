b1a6408e1fc6fe6eafb0af47bd19bfab4b0a6cab
wbc <wubaocheng@rtmap.com>
2021-07-20 14:05:56 +0800

    每日一题

---
+++ leet-code/src/com/lynx/everyday/MinPairSum.java
@@ -0,0 +1,73 @@
+package com.lynx.everyday;
+
+import com.lynx.common.annotition.MidCode;
+
+import java.util.Arrays;
+
+/**
+ * @Author cheng
+ * @Date 2021/7/20
+ * <p>
+ * 1877. 数组中最大数对和的最小值
+ * 一个数对 (a,b) 的 数对和 等于 a + b 。最大数对和 是一个数对数组中最大的 数对和 。
+ * <p>
+ * 比方说，如果我们有数对 (1,5) ，(2,3) 和 (4,4)，最大数对和 为 max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8 。
+ * 给你一个长度为 偶数 n 的数组 nums ，请你将 nums 中的元素分成 n / 2 个数对，使得：
+ * <p>
+ * nums 中每个元素 恰好 在 一个 数对中，且
+ * 最大数对和 的值 最小 。
+ * 请你在最优数对划分的方案下，返回最小的 最大数对和 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,5,2,3]
+ * 输出：7
+ * 解释：数组中的元素可以分为数对 (3,3) 和 (5,2) 。
+ * 最大数对和为 max(3+3, 5+2) = max(6, 7) = 7 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,5,4,2,4,6]
+ * 输出：8
+ * 解释：数组中的元素可以分为数对 (3,5)，(4,4) 和 (6,2) 。
+ * 最大数对和为 max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * n == nums.length
+ * 2 <= n <= 105
+ * n 是 偶数 。
+ * 1 <= nums[i] <= 105
+ * https://leetcode-cn.com/problems/minimize-maximum-pair-sum-in-array/
+ */
+@MidCode
+public class MinPairSum {
+
+    public int minPairSum(int[] nums) {
+        int n = nums.length;
+        int half = n / 2;
+        Arrays.sort(nums);
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < half; i++) {
+            res = Math.max(res, nums[i] + nums[n - 1 - i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // [3,5,2,3]
+        //[3,5,4,2,4,6]
+        MinPairSum m = new MinPairSum();
+        int[] nums = new int[]{3, 5, 2, 3};
+        System.out.println(m.minPairSum(nums) == 7);
+
+        nums = new int[]{3, 5, 4, 2, 4, 6};
+        System.out.println(m.minPairSum(nums) == 8);
+
+        //[4,1,5,1,2,5,1,5,5,4]
+        nums = new int[]{4, 1, 5, 1, 2, 5, 1, 5, 5, 4};
+        System.out.println(m.minPairSum(nums) == 8);
+    }
+}
