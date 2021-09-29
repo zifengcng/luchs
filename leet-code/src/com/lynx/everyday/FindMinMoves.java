24e084b214c7ad3424ddfe8c9b3df9ba63edad9a
wbc <wubaocheng@rtmap.com>
2021-09-29 18:53:19 +0800

    每日一题

---
+++ leet-code/src/com/lynx/everyday/FindMinMoves.java
@@ -0,0 +1,110 @@
+package com.lynx.everyday;
+
+import com.lynx.common.annotition.HardCode;
+
+import java.util.Arrays;
+
+/**
+ * @Author cheng
+ * @Date 2021/9/29
+ * <p>
+ * 517. 超级洗衣机
+ * 假设有 n 台超级洗衣机放在同一排上。开始的时候，每台洗衣机内可能有一定量的衣服，也可能是空的。
+ * <p>
+ * 在每一步操作中，你可以选择任意 m (1 <= m <= n) 台洗衣机，与此同时将每台洗衣机的一件衣服送到相邻的一台洗衣机。
+ * <p>
+ * 给定一个整数数组 machines 代表从左至右每台洗衣机中的衣物数量，请给出能让所有洗衣机中剩下的衣物的数量相等的 最少的操作步数 。如果不能使每台洗衣机中衣物的数量相等，则返回 -1 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：machines = [1,0,5]
+ * 输出：3
+ * 解释：
+ * 第一步:    1     0 <-- 5    =>    1     1     4
+ * 第二步:    1 <-- 1 <-- 4    =>    2     1     3
+ * 第三步:    2     1 <-- 3    =>    2     2     2
+ * 示例 2：
+ * <p>
+ * 输入：machines = [0,3,0]
+ * 输出：2
+ * 解释：
+ * 第一步:    0 <-- 3     0    =>    1     2     0
+ * 第二步:    1     2 --> 0    =>    1     1     1
+ * 示例 3：
+ * <p>
+ * 输入：machines = [0,2,0]
+ * 输出：-1
+ * 解释：
+ * 不可能让所有三个洗衣机同时剩下相同数量的衣物。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * n == machines.length
+ * 1 <= n <= 104
+ * 0 <= machines[i] <= 105
+ * https://leetcode-cn.com/problems/super-washing-machines/
+ */
+@HardCode
+public class FindMinMoves {
+
+    public int findMinMoves(int[] machines) {
+        int len = machines.length;
+        int sum = Arrays.stream(machines).sum();
+        if (sum % len != 0) {
+            return -1;
+        }
+
+        int avg = sum / len;
+        int res = 0;
+        int s = 0;
+        for (int num : machines) {
+            num -= avg;
+            s += num;
+            res = Math.max(res, Math.max(Math.abs(s), num));
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        FindMinMoves f = new FindMinMoves();
+
+        // 示例 1：
+        //
+        //输入：machines = [1,0,5]
+        //输出：3
+        //解释：
+        //第一步:    1     0 <-- 5    =>    1     1     4
+        //第二步:    1 <-- 1 <-- 4    =>    2     1     3
+        //第三步:    2     1 <-- 3    =>    2     2     2
+        int[] machines = new int[]{1, 0, 5};
+        System.out.println(f.findMinMoves(machines) == 3);
+
+
+        // 示例 2：
+        //
+        //输入：machines = [0,0,0,4]
+        //输出：3
+        machines = new int[]{0,0,0,4};
+        System.out.println(f.findMinMoves(machines) == 3);
+
+        // 示例 3：
+        //
+        //输入：machines = [3,1,0,4]
+        //输出：
+        machines = new int[]{3,1,0,4};
+        System.out.println(f.findMinMoves(machines));
+
+        // 示例 4：
+        //
+        //输入：machines = [100000,0,100000,0,100000,0,100000,0,100000,0,100000,0]
+        //输出：50000
+        machines = new int[]{100000,0,100000,0,100000,0,100000,0,100000,0,100000,0};
+        System.out.println(f.findMinMoves(machines) == 50000);
+
+
+    }
+}
